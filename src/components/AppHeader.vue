cb28ce38f18d494eabf1ec6fb26f9c77f9578108
DanieleBaggiano <danielebaggiano@gmail.com>
2024-05-02 18:04:44 +0200

    header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,10 +4,40 @@
 </script>
 
 <template>
-    <h1>miao</h1>
-
+    <div class="header">
+        <img class="logo-header" src="/src/assets/dc-logo.png" alt="">
+        <ul class="list-header">
+            <li>CHARACTERS</li>
+            <li>COMICS</li>
+            <li>MOVIES</li>
+            <li>TV</li>
+            <li>GAMES</li>
+            <li>COLLECTIBLES</li>
+            <li>VIDEOS</li>
+            <li>FANS</li>
+            <li>NEWS</li>
+            <li>SHOP</li>
+        </ul>
+    </div>
 </template>
 
 <style lang="scss">
+    .header {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        margin: 10px auto;
+        width: 70%;
+    }
 
+    .logo-header {
+        width: 60px;
+    }
+
+    .list-header {
+        display: flex;
+        column-gap: 20px;
+        list-style-type: none;
+        font-size: .6rem;
+    }
 </style>
