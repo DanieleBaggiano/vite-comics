ed3e60da85ad9510d7b221548bd3547765a33451
DanieleBaggiano <danielebaggiano@gmail.com>
2024-05-06 16:49:05 +0200

    props(?!)

--- src/components/AppCards.vue
+++ src/components/AppCards.vue
@@ -1,4 +1,6 @@
 <script>
+    import cardsArray from '../assets/cards';
+
     export default {
         data() {
             return {
@@ -7,86 +9,12 @@
                         image: "src/assets/jumbotron.jpg"
                     }
                 ],
-                cards: [
-                    {
-                        thumb: "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
-                        price: "$19.99",
-                        series: "Action Comics",
-                        type: "comic book",
-                    },
-                    {
-                        thumb: "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg",
-                        price: "$3.99",
-                        series: "American Vampire 1976",
-                        type: "comic book",
-                    },
-                    {
-                        thumb: "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
-                        price: "$16.99",
-                        series: "Aquaman",
-                        type: "graphic novel",
-                    },
-                    {
-                        thumb: "https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg",
-                        price: "$2.99",
-                        series: "Batgirl",
-                        type: "comic book",
-                    },
-                    {
-                        thumb: "https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg",
-                        price: "$3.99",
-                        series: "Batman",
-                        type: "comic book",
-                    },
-                    {
-                        thumb: "https://static.wikia.nocookie.net/marvel_dc/images/5/50/Batman_Beyond_v.1_1.jpg",
-                        price: "$2.99",
-                        series: "Batman Beyond",
-                        type: "comic book",
-                    },
-                    {
-                        thumb: "https://static.wikia.nocookie.net/marvel_dc/images/0/0d/Batman_Superman_Vol_1_1.jpg",
-                        price: "$3.99",
-                        series: "Batman/Superman",
-                        type: "comic book",
-                    },
-                    {
-                        thumb: "https://static.wikia.nocookie.net/marvel_dc/images/c/cf/Batman_Superman_Annual_Vol_2_1.jpg",
-                        price: "$4.99",
-                        series: "Batman/Superman Annual",
-                        type: "comic book",
-                    },
-                    {
-                        thumb: "https://static.wikia.nocookie.net/marvel_dc/images/5/54/Batman_The_Joker_War_Zone_Vol_1_1.jpg",
-                        price: "$5.99",
-                        series: "Batman: The Joker War Zone",
-                        type: "comic book",
-                    },
-                    {
-                        thumb: "https://static.wikia.nocookie.net/marvel_dc/images/6/64/Batman_Three_Jokers_Collected.jpg",
-                        price: "$6.99",
-                        series: "Batman: Three Jokers",
-                        type: "comic book",
-                    },
-                    {
-                        thumb: "https://static.wikia.nocookie.net/marvel_dc/images/f/f8/Batman_White_Knight_Presents_Harley_Quinn_Vol_1_1.jpg",
-                        price: "$4.99",
-                        series: "Batman: White Knight Presents: Harley Quinn",
-                        type: "comic book",
-                    },
-                    {
-                        thumb: "https://static.wikia.nocookie.net/marvel_dc/images/c/c8/Catwoman_Vol_2_1.jpg",
-                        price: "$16.99",
-                        series: "Catwoman",
-                        type: "graphic novel",
-                    },
-                ]
+                cardsArray,
             }
         },
-        methods: {
-            getImagePath(img) {
-                return new URL(`${img}`, import.meta.url).href;
-            }
+        props: {
+            image: String,
+            title: String,
         }
     }
 </script>
@@ -103,10 +31,11 @@
 
         <div class="bg-black">
             <div class="container-cards">
-                <div class="cards" v-for="item in cards">
-                    <img :src="getImagePath(item.thumb)" alt="">
-                    <h2>{{ item.series }}</h2>
+                <div class="cards" v-for="card in cardsArray">
+                    <img :src="card.thumb" alt="">
+                    <h2>{{ card.series }}</h2>
                 </div>
+
                 <button class="btn">LOAD MORE</button>
             </div>
         </div>
@@ -162,7 +91,7 @@
 
         .cards {
             width: calc(100% / 6 - 50px);
-            height: 200px;
+            height: 100px;
             object-fit: cover;
 
             img {
